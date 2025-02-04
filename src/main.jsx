c28ea57fe592f6aedacab4652280739d63e6cd5f
leongroth <leongroth@gmail.com>
2025-02-04 11:54:49 +0100

    router setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navbar from './components/Navbar.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Tasks from './components/Tasks.jsx'
+import Summary from './components/Summary.jsx'
+
+const router = createBrowserRouter([
+  {path: "/", element: <Tasks />},
+  {path: "/summary", element: <Summary />},
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
