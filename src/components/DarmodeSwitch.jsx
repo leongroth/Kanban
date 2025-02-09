7610ed83482fe213856c54d66f81d62710e12313
leongroth <leongroth@gmail.com>
2025-02-09 22:44:22 +0100

    Fixes

--- src/components/DarmodeSwitch.jsx
+++ src/components/DarmodeSwitch.jsx
@@ -19,15 +19,12 @@ const DarmodeSwitch = () => {
 
     return (
     <div className="mt-[10px] w-[90%] flex justify-end items-center gap-[5px]">
-        <button onClick={toggleTheme} className="w-[60px] h-[30px] bg-black dark:bg-white text-white dark:text-black rounded-full font-semibold drop-shadow-lg hover:bg-gray-700">
-            <div className='w-[26px] h-[26px] bg-white dark:bg-black rounded-full ml-[2px] transition-all duration-500 dark:ml-[32px] flex justify-center items-center'>
+        <button onClick={toggleTheme} className="w-[60px] h-[30px] bg-[#362E3E] dark:bg-white text-white dark:text-black rounded-full font-semibold drop-shadow-lg hover:bg-gray-700">
+            <div className='w-[26px] h-[26px] bg-white dark:bg-[#362E3E] rounded-full ml-[2px] transition-all duration-500 dark:ml-[32px] flex justify-center items-center'>
                 <img src={Sun} className='block dark:hidden' />
                 <img src={Moon} className='hidden dark:block' />
             </div>
         </button>
-        <div>
-            {JSON.parse(localStorage.getItem('isDarkmode'))}
-        </div>
     </div>
     )
 }
